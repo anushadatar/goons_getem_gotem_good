6fbc69c06f5abfec8b271248c4d3b8d5a68d8dec
Mark Goldwater <mark.goldwater@students.olin.edu>
2017-12-02 04:40:00 -0500

    I deleted stuff...don't worry about it

--- titleAbstractor.py
+++ titleAbstractor.py
@@ -13,7 +13,7 @@ import grammar_check
 
 
 
-url = "https://www.wired.com/story/uber-waymo-gm-infiniti-lucid/"
+url = "https://www.cbsnews.com/news/walmart-pulls-rope-tree-journalist-t-shirt-from-site/"
 parser = HtmlParser.from_url(url, Tokenizer("english"))
 # or for plain text files
 # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
@@ -25,17 +25,5 @@ summarizer.stop_words = get_stop_words("english")
 search = ""
 
 for sentence in summarizer(parser.document, 1):
+    print(sentence)
     sentence = str(sentence)
-    tokens = nltk.word_tokenize(sentence)
-    tagged = nltk.pos_tag(tokens)
-
-    for element in tagged:
-        if element[1] == 'NNP' or element[1] == 'NNPS' or element[1] == 'NN' or element[1] == 'VB' or element[1] == 'VBD' or element[1] == 'VBG' or element[1] == 'VBN' or element[1] == 'VBP' or element[1] == 'VBZ':
-            search += element[0] + " "
-
-search = str(search)
-
-tool = grammar_check.LanguageTool('en-GB')
-matches = tool.check(search)
-new = grammar_check.correct(search, matches)
-print(search)
